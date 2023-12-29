da89093e5e537f97b92c3be620683d8d163332d1
LeeHanBit <lhb0269@gmail.com>
2023-12-29 16:47:06 +0900

    [level 3] Title: 등굣길, Time: 0.06 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -4,22 +4,22 @@
 using namespace std;
 
 int solution(int m, int n, vector<vector<int>> puddles) {
-    vector<vector<int>> v(n, vector<int>(m, 0));
-    vector<vector<int>> puddllist(n, vector<int>(m, 0));
+    vector<vector<int>>v(n, vector<int>(m, 0));
+    vector<vector<int>>puddlist(n, vector<int>(m, 0));
     for (auto& a : puddles) {
-        puddllist[a[1] - 1][a[0] - 1] = 1;
+        puddlist[a[1] - 1][a[0] - 1] = 1;
     }
     v[0][0] = 1;
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
-            if (puddllist[i][j] == 1)
+            if (puddlist[i][j] == 1)
                 v[i][j] = 0;
             else {
-                if (j > 0) {
-                    v[i][j] += (v[i][j - 1] % 1000000007);
-                }
                 if (i > 0) {
                     v[i][j] += (v[i - 1][j] % 1000000007);
+                }
+                if (j > 0) {
+                    v[i][j] += (v[i][j-1] % 1000000007);
                 }
             }
         }
