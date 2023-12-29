027ff8063f40c5a2ce8b0d983a86255100b956a0
LeeHanBit <lhb0269@gmail.com>
2023-12-29 22:01:33 +0900

    [level 3] Title: 등굣길, Time: 0.07 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -4,25 +4,26 @@
 using namespace std;
 
 int solution(int m, int n, vector<vector<int>> puddles) {
-    vector<vector<int>>v(n, vector<int>(m, 0));
-    vector<vector<int>>puddlist(n, vector<int>(m, 0));
-    for (auto& a : puddles) {
-        puddlist[a[1] - 1][a[0] - 1] = 1;
-    }
-    v[0][0] = 1;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            if (puddlist[i][j] == 1)
-                v[i][j] = 0;
-            else {
-                if (i > 0) {
-                    v[i][j] += (v[i - 1][j] % 1000000007);
-                }
-                if (j > 0) {
-                    v[i][j] += (v[i][j-1] % 1000000007);
-                }
-            }
-        }
-    }
-    return v[n-1][m-1] % 1000000007;
+	vector<vector<int>>dp(n, vector<int>(m, 0));
+	vector<vector<int>>a(n, vector<int>(m, 0));
+
+	for (auto& v : puddles) {
+		a[v[1] - 1][v[0] - 1] = 1;
+	}
+
+	dp[0][0] = 1;
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			if (a[i][j] == 1)
+				dp[i][j] = 0;
+			else {
+				if (i > 0)
+					dp[i][j] += (dp[i - 1][j] % 1000000007);
+				if (j > 0)
+					dp[i][j] += (dp[i][j - 1] % 1000000007);
+			}
+		}
+	}
+	return dp[n-1][m-1] % (1000000007);;
 }
