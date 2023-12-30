9bb2253f094d2731b280df6bfe2b431f87d5fe7b
LeeHanBit <lhb0269@gmail.com>
2023-12-30 23:07:14 +0900

    Update 카펫.cpp

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -18,3 +18,29 @@ vector<int> solution(int brown, int yellow) {
     }
     return answer;
 }
+///////////////////////////////////////////////////////////////////////////////
+//약수를 구하는 방식으로 접근했었음 테스트 케이스 4,6,7번 실패로 다른 방법으로 접근//
+///////////////////////////////////////////////////////////////////////////////
+vector<int> answer;
+vector<int>num;
+
+for (int i = 1; i <= brown + yellow; ++i) {
+    if ((brown + yellow) % i == 0) {
+        num.push_back(i);
+    }
+}
+int curr = num.size() / 2;
+for (int i = 0; i <= curr; ++i) {
+    if (num[curr] * num[curr + i] == brown + yellow) {
+        
+        answer.push_back(num[curr+i]);
+        answer.push_back(num[curr]);
+        return answer;
+    }
+    if (num[curr] * num[curr - i] == brown + yellow) {
+        answer.push_back(num[curr]);
+        answer.push_back(num[curr - i]);
+        return answer;
+    }
+}
+///////////////////////////////////////////////////
