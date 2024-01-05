7552f7bb7582c1d536478236271657eafc2c4db8
LeeHanBit <lhb0269@gmail.com>
2024-01-06 02:00:26 +0900

    [level 2] Title: 피보나치 수, Time: 0.86 ms, Memory: 3.84 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -9,9 +9,7 @@ int solution(int n) {
     v.push_back(0);
     v.push_back(1);
     for (int i = 2; i <= n; ++i) {
-        int curr = (v[i - 1] + v[i - 2]) % 1234567;
-        v.push_back(curr);
+        v.push_back((v[i - 2] % 1234567) + (v[i - 1]%1234567));
     }
-    cout << v[n] % 1234567;
-    return v[n]% 1234567;
+    return v[n] % 1234567;
 }
